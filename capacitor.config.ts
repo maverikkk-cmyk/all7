a808ce8d257267024b0811621dd254eecfc7e723
maverikkk-cmyk <maverikkk@gmail.com>
2026-07-04 11:32:22 +0530

    chore: add capacitor.config.ts with webDir=dist

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maverikkk.all7',
+  appName: 'all7',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
